925d25a751dac41cffe96a680f14979a42834a5e
Akinori Tachibana <akinoritachibana@Akinorinonotobukkukonpyuta.local>
2025-04-30 23:00:39 +0900

    fix: relax params type to allow build on Next.js 15

--- src/app/entries/[date]/page.tsx
+++ src/app/entries/[date]/page.tsx
@@ -1,11 +1,10 @@
 // src/app/entries/[date]/page.tsx
-import { use } from 'react';
+
 import { parseISO, format } from 'date-fns';
 import { ja } from 'date-fns/locale';
 import EntryClientPage from './EntryClientPage';
 
-export default function EntriesPage(promise: Promise<{ params: { date: string } }>) {
-  const { params } = use(promise);
+export default async function EntriesPage({ params }: any) {
   const { date } = params;
   const parsedDate = parseISO(date);
   const formattedDate = format(parsedDate, 'yyyy年M月d日 (EEEE)', { locale: ja });
